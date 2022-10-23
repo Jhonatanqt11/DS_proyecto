7fcfe1d061e9e8a77143c5e0f0f2d95ea38d0b81
Jhonatanqt11 <101925098+Jhonatanqt11@users.noreply.github.com>
2022-10-23 14:32:43 +0200

    task1

--- TimeTracker/src/Tasks.java
+++ TimeTracker/src/Tasks.java
@@ -1,20 +1,22 @@
 import javax.crypto.Cipher;
 import java.time.Duration;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Tasks extends Composite {
-  private List<Interval> Interval;
+  private List<Interval> interval;
   private DateTimeFormatter initD;
   private DateTimeFormatter finalD;
 
   public Tasks(String n){
     super(n);
+    interval = new ArrayList<Interval>();
   }
   public Duration TotalTime() {
-    Duration duration = Duration.between(Interval.get(0).getInitD(), Interval.get(0).getFinalD());
-    for(int i = 1; i< Interval.size(); i++){
-      Duration duration1 = Duration.between(Interval.get(i).getInitD(), Interval.get(i).getFinalD());
+    Duration duration = Duration.ZERO;
+    for(int i = 0; i< interval.size(); i++){
+      Duration duration1 = Duration.between(interval.get(i).getInitD(), interval.get(i).getFinalD());
       duration=duration.plus(duration1);
     }
     return duration;
@@ -22,4 +24,8 @@ public class Tasks extends Composite {
 
   public DateTimeFormatter getInitD() {return initD;}
   public DateTimeFormatter getFinalD() {return finalD;}
+  public void addInterval(Interval i){
+    interval.add(i);
+  }
+
 }
