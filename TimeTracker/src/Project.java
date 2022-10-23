7b45229e6544cced8e0d4b463aad8ac8ab662948
Pol Gonzalez Casals 1605595 <polgoca@gmail.com>
2022-10-23 14:21:03 +0200

    Update Project.java

--- TimeTracker/src/Project.java
+++ TimeTracker/src/Project.java
@@ -1,17 +1,39 @@
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-public class Project extends Composite {
-  private String name;
+import java.util.Date;
+import java.util.List;
+import java.time.Duration;
 
-  public Project(String n)
-  {
+public class Project extends Composite {
+
+  private List<Composite> Composites;
+
+  public Project(String n) {
     super(n);
   }
 
-  public DateTimeFormatter TotalTime(){
+  public Duration TotalTime() {
 
-    return null;
+    for(int i=0; i<Composites.size(); i++)
+    {
+
+      if(Composites.get(i).esTask())
+      {
+
+      }
+      else
+      {
+
+      }
+    }
   }
 
-  public String getName() {return name;}
+  public void addComposite(Composite t) {
+    Composites.add(t);
+  }
 
+  public boolean removeComposite(Composite t) {
+    return Composites.remove(t);
+    //Intento de if(*eliminado*) return true, if(*no eliminado*) return false
+  }
 }
