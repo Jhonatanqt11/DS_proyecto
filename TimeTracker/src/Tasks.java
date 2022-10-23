99fc8ed315c6bb63e970f3aa1acf970b7d2c47ab
Jhonatanqt11 <101925098+Jhonatanqt11@users.noreply.github.com>
2022-10-23 12:31:22 +0200

    task_composite

--- TimeTracker/src/Tasks.java
+++ TimeTracker/src/Tasks.java
@@ -1,12 +1,15 @@
+import javax.crypto.Cipher;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-public class Tasks extends Project{
+public class Tasks extends Composite {
   private List<Interval> Interval;
   private DateTimeFormatter initD;
   private DateTimeFormatter finalD;
 
-  public Tasks(String n);
+  public Tasks(String n){
+    super(n);
+  }
   public DateTimeFormatter TotalTime();
   public DateTimeFormatter getInitD() {return initD;}
   public DateTimeFormatter getFinalD() {return finalD;}
